0adffe587a0c0e7c331b6bd679f60206d2466b81
HigorMatheus <hmoliveira83@gmail.com>
2020-09-16 13:11:53 -0300

    add front

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import Product from './components/producto';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <Product/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/producto/index.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import './style.css'
+import api from '../../services/api';
+
+interface Iproducts{
+    id: Number,
+    name: String,
+    image: String,
+    description:String,
+    cost: String,
+    image_url: string
+
+}
+// import './Product.css';
+const Product = ()=>{
+    const [product, setProduct] = useState([])
+
+    useEffect(()=>{
+     api.get('/product').then( response=>{
+        const products =response.data;
+        // console.log(photos);
+         setProduct(products)
+     })
+    },[])
+    return(
+        <div className="Product d-flex flex-row bd-highlight mb-3">
+                     {product.map((prod :Iproducts)=>{
+                         
+                         
+                         return(
+                            <div className="  d-flex card col-md-4" >
+                            <img src={prod.image_url} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                         <h5 className="card-title">{prod.name}</h5>
+                                <p className="card-text">{prod.description}</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                         )
+                     })}
+                
+        </div>
+    )
+}
+
+// class Product extends Component {
+//     state={
+//         products:[]
+//     }
+    
+//     componentDidMount(){
+//         this.loadProducs()
+//       }
+      
+//       loadProducs = async( )=>{
+//         const response = await api.get(`/product`)
+//         const { products, ...productInfo}= response.data
+//         this.setState({products, productInfo})
+//       }
+
+//     render(){
+//         const {products} = this.state
+//           return (
+
+//             <div className="Product">
+//                 {products.map()}
+//             <img src="" alt=""/>
+//             <p>descricao</p>
+//             </div>
+//         );
+//     }
+
+// }
+
+export default Product;
